76f1fddf2e0439f7693acbad9676d8bbeb76ded0
milan-murray <milan-murray@outlook.ie>
2022-12-18 02:42:14 +0100

    Updated content, styles
    Added pictures
    Added education details
    Updated navbar
    Added ScrollSpy

--- sources/scripts.js
+++ sources/scripts.js
@@ -1,1 +1,16 @@
 document.getElementById("ReturnToTopBtn").style.display = "none";
+
+// Get the button
+let scrollTopBtn = document.getElementById("ReturnToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+}
+
